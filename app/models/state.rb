e7b8486104052ca672d4e0675c85e99d4947fe87
kroteDev <kroteDev@users.noreply.github.com>
2016-05-05 11:59:50 -0300

    Update state.rb
    Added all States From Brasil.

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,6 +5,6 @@ class State < ActiveRecord::Base
   has_many :cities
 
   def self.array
-    @array ||= order(:name).pluck(:name, :acronym).push(['Outro / Other', 'outro / other'])
+    @array ||= order(:name).pluck(:name, :acronym).push(['Acre','AC'],['Alagoas','AL'],['Amapá','AP'],['Amazonas','AM'],['Bahia ','BA'],['Ceará','CE'],['Distrito Federal ','DF'],['Espírito Santo','ES'],['Goiás','GO'],['Maranhão','MA'],['Mato Grosso','MT'],['Mato Grosso do Sul','MS'],['Minas Gerais','MG'],['Pará','PA'],['Paraíba','PB'],['Paraná','PR'],['Pernambuco','PE'],['Piauí','PI'],['Rio de Janeiro','RJ'],['Rio Grande do Norte','RN'],['Rio Grande do Sul','RS'],['Rondônia','RO'],['Roraima','RR'],['Santa Catarina','SC'],['São Paulo','SP'],['Sergipe','SE'],['Tocantins','TO'])
   end
 end
